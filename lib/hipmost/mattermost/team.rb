0033c1ec7da0ffd3349dd6051c19ebb8dc72dfb1
Gabriel Rios <gabrielfalcaorios@gmail.com>
2018-07-22 18:01:14 -0300

    Fix team format

--- lib/hipmost/mattermost/team.rb
+++ lib/hipmost/mattermost/team.rb
@@ -7,11 +7,15 @@ module Hipmost
       attr_reader :display_name
 
       def to_jsonl
-        %[{ "type": "team", "team": { "display_name": "#@display_name", "diplay_display_name": "#@display_name", "type": "I", "description": "#@display_name", "allow_open_invite": false } }]
+        %[{ "type": "team", "team": { "display_name": "#@display_name", "name": "#{name}", "type": "I", "description": "#@display_name", "allow_open_invite": false } }]
       end
 
       def name
-        return "oi" if display_name == "Orbital Impact"
+        if display_name == "Orbital Impact"
+          "oi"
+        else
+          name
+        end
       end
     end
   end
