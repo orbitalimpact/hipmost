891e304f08755d1b0644eab73263f0340675ba8d
Daniel Rauer <rauer@bytemine.net>
2019-06-03 22:37:25 +0200

    Proper error message if no command is specified.

--- lib/hipmost.rb
+++ lib/hipmost.rb
@@ -7,15 +7,20 @@ require "hipmost/cmds"
 module Hipmost
   class CLI
     def self.run(command, args, options)
-      case command.to_sym
-      when :users
-        Hipmost::Cmds::Users.new(**options).run(args)
-      when :public, :room, :rooms
-        Hipmost::Cmds::Room.new(**options).run(args)
-      when :private, :direct
-        Hipmost::Cmds::Private.new(**options).run(args)
+      if command
+        case command.to_sym
+        when :users
+          Hipmost::Cmds::Users.new(**options).run(args)
+        when :public, :room, :rooms
+          Hipmost::Cmds::Room.new(**options).run(args)
+        when :private, :direct
+          Hipmost::Cmds::Private.new(**options).run(args)
+        else
+          puts "Invalid command"
+          exit 1
+        end
       else
-        puts "Invalid command"
+        puts "A command must be specified"
         exit 1
       end
     end
